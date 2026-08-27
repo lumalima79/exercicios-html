0ed2556052206bebccd25da68ca88532f9c34e5a
lumalima79 <lonnance@hotmail.com>
2026-08-27 12:10:08 -0300

    Add files via upload

--- scriptexerc10.js
+++ scriptexerc10.js
@@ -7,19 +7,31 @@ let resposta = document.querySelector("#resposta");
 botao.addEventListener("click",calcular);
 
 function calcular(){
-    const valor1 = parseInt(campo1.value);
-    const valor2 = parseInt(campo2.value);
 
-    if (selector.value === "soma"){
-        resposta.innerHTML = valor1 + valor2;
+    if(campo1.value==='' || campo2.value===''   ){
+       resposta.classList.add("problema");
+       resposta.innerHTML = "campo vazio";
+       setTimeout(() => {
+        resposta.classList.remove("problema");
+        resposta.innerHTML = "";
+       }, 2000);
+
     }
-    if (selector.value === "subtracao"){
-        resposta.innerHTML = valor1 - valor2;
-    }
-    if (selector.value === "multiplicacao"){
-        resposta.innerHTML = valor1 * valor2;
-    }
-    if (selector.value === "divisao"){
-        resposta.innerHTML = valor1 / valor2;
+    else{
+        const valor1 = parseInt(campo1.value);
+        const valor2 = parseInt(campo2.value);
+
+        if (selector.value === "soma"){
+            resposta.innerHTML = valor1 + valor2;
+        }
+        if (selector.value === "subtracao"){
+            resposta.innerHTML = valor1 - valor2;
+        }
+        if (selector.value === "multiplicacao"){
+            resposta.innerHTML = valor1 * valor2;
+        }
+        if (selector.value === "divisao"){
+            resposta.innerHTML = valor1 / valor2;
+        }
     }
 }
